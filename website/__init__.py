d3b500354f33c33a1d884fdf7efdec3f98cb1067
Séraphin Vandegar <sva@opinum.com>
2019-02-28 15:13:41 -0800

    move secret_key to env variable

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
 
 # TODO : remove this key from here
-app.secret_key = 'foo'
+app.secret_key = os.environ['SECRET_KEY']
 
 from website import *
 import backend
