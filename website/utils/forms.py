558333b1905238cc18e35acf7788aef8ca89c06b
Séraphin Vandegar <sva@opinum.com>
2019-03-01 14:51:35 -0800

    fix the display

--- website/utils/forms.py
+++ website/utils/forms.py
@@ -2,8 +2,8 @@ from wtforms import Form, validators, DateTimeField, IntegerField
 import datetime
 
 class InputForm(Form):
-    start = DateTimeField('Departure', default=datetime.datetime.now(), format= '%Y-%m-%d %H:%M')
-    end = DateTimeField('Return ',default=datetime.datetime.now(), format = '%Y-%m-%d %H:%M')
+    start = DateTimeField( '',default=datetime.datetime.now(), format= '%Y-%m-%d %H:%M')
+    end = DateTimeField('',default=datetime.datetime.now(), format = '%Y-%m-%d %H:%M')
     distance = IntegerField('Distance [km]', [validators.DataRequired()])
     passengers = IntegerField('Number of passengers',
                               [validators.DataRequired(),
